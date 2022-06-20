4d6c2462a0a29c20692cee419cc60d468e8fc704
stephjoseph <sjtmunez@gmail.com>
2022-06-20 13:45:20 +0800

    Create index.php

---
+++ php-103/exam1/index.php
@@ -0,0 +1,15 @@
+
+<?php 
+include "function.php";
+
+?>
+
+<html>
+<body>
+ 
+<form action="index.php" method="post">
+Enter a number: <input type="number" name="inputNumber"><br>
+<input type="submit">
+</form>
+</body>
+</html>
